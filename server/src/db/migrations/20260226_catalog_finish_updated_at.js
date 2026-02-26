e3175b2846d048184552c627caa2cbbbff9817e3
Pedro Lopes <pedrorfmlopes@gmail.com>
2026-02-26 16:21:12 +0000

    fix: add missing updated_at to catalog_finishes

---
+++ server/src/db/migrations/20260226_catalog_finish_updated_at.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('catalog_finishes', table => {
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('catalog_finishes', table => {
+        table.dropColumn('updated_at');
+    });
+};
